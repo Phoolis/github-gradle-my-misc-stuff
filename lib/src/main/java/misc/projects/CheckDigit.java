42340b59d148240e0319b752b2f3911bedb27da4
Phoolis <carlsonps@gmail.com>
2024-04-11 15:04:03 +0300

    add CheckDigit and appendCheckDigit methods and respective tests

--- lib/src/main/java/misc/projects/CheckDigit.java
+++ lib/src/main/java/misc/projects/CheckDigit.java
@@ -1,22 +1,53 @@
 package misc.projects;
 
 /**
-     * Generate a check digit to given number code (string).
+     * Generate a EAN-13 check digit to given number code.
      *
-     * The check digit is an additional digit, used to verify that a barcode has been scanned correctly. 
+     * "The check digit is an additional digit, used to verify that a barcode has been scanned correctly. 
      * It is computed modulo 10, where the weights in the checksum calculation alternate 3 and 1. In particular, 
      * since the weights are relatively prime to 10, the EAN-13 system will detect all single digit errors. It 
-     * also recognizes 90% of transposition errors (all cases, where the difference between adjacent digits is not 5). 
-     * (from Wikipedia: https://en.wikipedia.org/wiki/International_Article_Number)
+     * also recognizes 90% of transposition errors (all cases, where the difference between adjacent digits is not 5)."
+     * [Wikipedia EAN-13: https://en.wikipedia.org/wiki/International_Article_Number]
      *
-     * @param code the price to number code, as a string
-     * @return the number code suffixed with the check digit as a string
+     * @param code the number code
+     * @return the number code suffixed with the check digit
      */
 
 public class CheckDigit {
 
-    public String addCheckDigit(String code) {
-        return "";
+    public int calculateCheckDigit(int code) {
+        
+        // Calculates the check digit for given number code.
+        // Digits are numbered from right to left, starting with index 1.
+        // Odd indexed digits are tripled and added to the sum,.
+        // Even indexed digits are added to the sum.
+        // Modulo 10 of the sum is the check digit.
+
+        String numStr = String.valueOf(code);
+        char[] charArray = numStr.toCharArray();
+        char[] reversed = new char[charArray.length];
+        for (int i = 0; i < charArray.length; i++) {
+            reversed[charArray.length -i -1] = charArray[i];
+        }
+
+        int sum = 0;
+        for (int i =0; i < reversed.length; i++) {
+            if ((i+1) % 2 == 0) {
+                sum += Character.getNumericValue(reversed[i]);
+            }
+            else {
+                sum += 3 * Character.getNumericValue(reversed[i]);
+            }
+        }
+        char checkDigit = (char) (sum % 10);
+        return checkDigit;
+    }
+
+    public int appendCheckDigit(int code) {
+        int barcode;
+        int checkDigit = calculateCheckDigit(code);
+        barcode = code * 10 + checkDigit;
+        return barcode;
     }
 
 }
--- lib/src/test/java/misc/projects/CheckDigitTest.java
+++ lib/src/test/java/misc/projects/CheckDigitTest.java
@@ -2,15 +2,67 @@ package misc.projects;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class CheckDigitTest {
+    private CheckDigit digitter;
+
+    @BeforeEach
+    void setupClass() {
+        digitter = new CheckDigit();
+    }
 
     @Test
     void testClassInitialization() {
-        CheckDigit digitter = new CheckDigit();
         assertTrue(digitter != null, "Created class should not be null");
         assertTrue(digitter instanceof CheckDigit, "Created class should be an instance of CheckDigit");
     }
+    
+    @Test
+    void codeOneReturnsThree() {
+        assertEquals(3, digitter.calculateCheckDigit(1));
+    }
 
+    @Test
+    void codeTwoReturnsSix() {
+        assertEquals(6, digitter.calculateCheckDigit(2));
+    }
+
+    @Test
+    void codeOneOneReturnsFour() {
+        assertEquals(4, digitter.calculateCheckDigit(11));
+    }
+
+    @Test
+    void codeOneTwoReturnsSeven() {
+        assertEquals(7, digitter.calculateCheckDigit(12));
+    }
+
+    @Test
+    void codeTwoOneReturnsThree() {
+        assertEquals(5, digitter.calculateCheckDigit(21));
+    }
+
+    @Test
+    void codeOneTwoThreeReturnsFour() {
+        // 123 -> 3*3 + 2 + 3*1 = 14
+        // 14 % 10 = 4
+        assertEquals(4, digitter.calculateCheckDigit(123));
+    }
+
+    @Test
+    void appendFourWhenGivenCodeOneTwoThree() {
+        assertEquals(1234, digitter.appendCheckDigit(123));
+    }
+    @Test
+    void appendFiveWhenGivenCodeOneThreeThree() {
+        assertEquals(1335, digitter.appendCheckDigit(133));
+    }
+
+//     @Test
+//     void appendSevenWhenGivenCode400638133393() {
+//         //TODO: switch from int to string, since long ints cause problems
+//         assertEquals(400638133393, digitter.appendCheckDigit(4006381333931));
+//     }
 }
